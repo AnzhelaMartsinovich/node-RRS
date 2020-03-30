36ee2886255794f27a3bb7abeb6e656899cd530c
Anzhela Martsinovich <anzhela.martsinovich@gmail.com>
2020-03-30 23:53:50 +0300

    feat: add file letters

--- my_caesar_cli/helpers/caesarCipher.js
+++ my_caesar_cli/helpers/caesarCipher.js
@@ -1,60 +1,6 @@
+const { lowerLetters, upperLetters } = require('./letters');
+
 const caesarCipher = (strInput, shift, action) => {
-  const lowerLetters = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-  ];
-  const upperLetters = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z'
-  ];
   const arrInput = strInput.split('');
   const pushLetter = [];
 
